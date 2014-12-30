d8acd393db02c228f02c821dbdde2384cdbcd7a8
Yang Fei <249893977@qq.com>
2014-12-30 17:59:43 +0800

    Create a Survival Game

---
+++ Assets/Scripts/EnemyLogic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLogic : MonoBehaviour {
+
+	public int Health = 100;
+	
+	// Update is called once per frame
+	private void Update () {
+		if (Health <= 0)
+		{
+			Dead();
+		}
+	}
+
+	private void Dead(){
+		Destroy (gameObject);
+	}
+
+	public void ApplyDamage(int theDamage){
+		Health -= theDamage;
+	}
+}
---
+++ Assets/Scripts/MeleeScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeScript : MonoBehaviour {
+	public int TheDamage = 50;
+	public float MaxDistance = 1.5f;
+	public float distance;
+	public Transform TheMace;
+
+	
+	// Update is called once per frame
+	private void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			TheMace.animation.Play("Attack");
+
+			RaycastHit hit = new RaycastHit();
+			if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit)){
+				distance = hit.distance;
+				if (distance <= MaxDistance){
+					hit.transform.SendMessage("ApplyDamage", TheDamage , SendMessageOptions.DontRequireReceiver);
+				}
+			}
+		}
+	}
+}
